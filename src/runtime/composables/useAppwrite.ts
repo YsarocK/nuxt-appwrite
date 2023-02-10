bba979f39a83ec56608f26bb3c3aa161ff3718bb
Etienne <emoureton@gmail.com>
2023-02-10 22:18:22 +0100

    cleaned conflict

--- src/runtime/composables/useAppwrite.ts
+++ src/runtime/composables/useAppwrite.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { useNuxtApp } from "#app";
-=======
 import { useNuxtApp } from '#app'
->>>>>>> cba11edad02e22333e75bf99efe2e20127ceceea
 
 const useAppwrite = () => {
   const { $appwrite } = useNuxtApp();
