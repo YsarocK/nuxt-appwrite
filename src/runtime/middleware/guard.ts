3a0d8a202fa60df31e88574c1178556f8b5d93d1
Yasin Ergün <yasin@ergn.org>
2023-02-28 00:42:14 +0300

    Update guard.ts

--- src/runtime/middleware/guard.ts
+++ src/runtime/middleware/guard.ts
@@ -2,8 +2,9 @@ import { defineNuxtRouteMiddleware, navigateTo } from '#app'
 import useAppwrite from '../composables/useAppwrite'
 
 export default defineNuxtRouteMiddleware(async () => {
-  const user = await useAppwrite().account.get()
-  if (!user.$id) {
+  try {
+    await useAppwrite().account.get();
+  } catch {
     return navigateTo('/login')
   }
   return
