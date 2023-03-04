8a204a7ed5ba4d59f20b99f32414079f6b7663ab
Etienne <emoureton@gmail.com>
2023-03-04 14:11:51 +0100

    fix undefined useAppwrite

--- src/runtime/composables/useAuthSession.ts
+++ src/runtime/composables/useAuthSession.ts
@@ -1,15 +1,16 @@
+import { useNuxtApp } from "#app";
 import { Models } from "node-appwrite";
 
 /**
  * Use the current Appwrite session
  */
 const useAuthSession = () => {
-  const appwrite = useAppwrite();
+  const { $appwrite } = useNuxtApp();
   const session = ref<Models.Account<Models.Preferences> | undefined | null>(null)
 
   onBeforeMount(async () => {
     try {
-      session.value = await appwrite.account.get();
+      session.value = await $appwrite.account.get();
     } catch (e) {
       session.value = undefined
     }
