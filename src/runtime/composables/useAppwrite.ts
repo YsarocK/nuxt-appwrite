f1bded3508681847554b63acd7edadf863db1954
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-31 11:42:34 +0200

    fix types

--- src/runtime/composables/useAppwrite.ts
+++ src/runtime/composables/useAppwrite.ts
@@ -1,10 +1,13 @@
 import { useNuxtApp } from '#app'
+import { Appwrite } from "../plugins/appwrite";
+
 /**
  * Use the current Appwrite instance
  * @returns Client, Databases, Account, Storage, Avatars, Functions, Role, Permission
  */
 const useAppwrite = () => {
-  const { $appwrite } = useNuxtApp();
+  const app = useNuxtApp();
+  const $appwrite: Appwrite = app.$appwrite;
   return $appwrite;
 }
 
